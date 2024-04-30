d229cd0491126503da2a4858c01f622007204753
Michele Awada <micheleawada25@gmail.com>
2024-04-30 23:34:04 +0300

    added maizle

---
+++ maizzle/config.js
@@ -0,0 +1,31 @@
+/** @type {import('@maizzle/framework').Config} */
+
+/*
+|-------------------------------------------------------------------------------
+| Development config                      https://maizzle.com/docs/environments
+|-------------------------------------------------------------------------------
+|
+| The exported object contains the default Maizzle settings for development.
+| This is used when you run `maizzle build` or `maizzle serve` and it has
+| the fastest build time, since most transformations are disabled.
+|
+*/
+
+module.exports = {
+  baseDevFullStaticProdUrl: "/images/social-media-icons/",
+  build: {
+    components: {
+      tagPrefix: 'x-',
+    },
+    templates: {
+      source: 'src/templates',
+      destination: {
+        path: 'build_local',
+      },
+      assets: {
+        source: 'src/images',
+        destination: 'images',
+      },
+    },
+  },
+}
