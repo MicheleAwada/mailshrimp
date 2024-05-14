7f7ee25936ee349750a19b398a9caa11fa6895e7
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-05-14 20:37:58 +0200

    added get user data from user method

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,15 +1,33 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.authtoken.views import ObtainAuthToken
 from .models import User
 from rest_framework.authtoken.models import Token
+from rest_framework import permissions
 
 from django.utils.translation import gettext_lazy as _
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from django.contrib.auth import authenticate
 from rest_framework import serializers
 
+def getTokenFromUser(user):
+    token, _ = Token.objects.get_or_create(user=user)
+    return token.key
+
+
+
+def getUserDataFromUser(user):
+    return {
+        "token": getTokenFromUser(user),
+        "username": user.username,
+    }
+
+@api_view(["GET"])
+@permission_classes([permissions.IsAuthenticated])
+def getUserData(request):
+    user_data = getUserDataFromUser(request.user)
+    return Response(user_data)
 
 class CustomMessageAuthTokenSerializer(AuthTokenSerializer):
     def validate(self, attrs):
