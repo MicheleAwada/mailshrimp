b395b3575782cb5549c779b36ed2ae0e2a517d12
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-05-24 07:46:57 +0200

    mailbox serializer

---
+++ backend/mail_accounts/serializers.py
@@ -0,0 +1,19 @@
+class MailBoxSerializer(serializers.ModelSerializer):
+    domain = DomainForeignKeyField(queryset=models.Domain.objects.all())
+    def validate(self, attrs):
+        quota = attrs.get("quota")
+        max_mailbox_quota = models.MailBox(domain=attrs.get("domain"), quota=attrs.get("quota"), ).get_max_mailbox_quota(exclude=False) # exclude=False since its not saved
+        if quota > max_mailbox_quota:
+            raise serializers.ValidationError("MailBox Quota must not be greater than Domain's Max MailBox Quota")
+        return attrs
+    def validate_password(self, value):
+        fake_model = models.MailBox(password=value)
+        fake_model.set_password(value)
+        return fake_model.password
+
+    class Meta:
+        model = models.MailBox
+        fields = ["fullname", "username", "quota", "password", "domain"]
+        extra_kwargs = {
+            "password": {"write_only": True},
+        }
