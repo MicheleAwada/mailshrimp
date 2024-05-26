3e6bd6eefd52a14c3238939702b0d6ef89c4e410
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-05-26 15:52:15 +0200

    domain serializer

--- backend/mail_accounts/serializers.py
+++ backend/mail_accounts/serializers.py
@@ -1,3 +1,36 @@
+class DomainSerializer(serializers.ModelSerializer):
+    def validate(self, attrs):
+        quota = attrs.get("quota")
+        max_mailbox_quota = attrs.get("max_mailbox_quota")
+        if max_mailbox_quota > quota:
+            raise serializers.ValidationError("Domain's Max MailBox Quota must not be greater than Domain Quota")
+
+        instance = self.instance
+        domain_name = attrs.get("name")
+        dkim_selector = attrs.get("dkim_selector")
+        dkim_key_size = attrs.get("dkim_key_size")
+        created = instance is None
+        dkim_options_changed =  not created and (dkim_selector != instance.dkim_selector or dkim_key_size != instance.dkim_key_size)
+        need_to_change_dkim = bool(created or dkim_options_changed)
+        if need_to_change_dkim:
+            dkim_dictonary_value = set_dkim(dkim_selector=dkim_selector, dkim_key_size=dkim_key_size, domain_name=domain_name)
+            for key, value in dkim_dictonary_value.items():
+                attrs[key] = value
+        return attrs
+
+
+    quota = IntegerOrInfinityField()
+    max_mailbox_quota = IntegerOrInfinityField()
+    max_mailboxes = IntegerOrInfinityField()
+    max_aliases = IntegerOrInfinityField()
+    class Meta:
+        model = models.Domain
+        fields = [*full_dkim_fields, "name", "quota", "max_mailbox_quota", "max_mailboxes", "max_aliases"]
+        read_only_fields = [*full_dkim_read_only_fields]
+
+
+
+
 class DomainForeignKeyField(serializers.RelatedField):
     def to_representation(self, value):
         return value.id
