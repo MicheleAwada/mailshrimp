45fc4338d59fb870d3930c357e6d08518c70e7bf
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-06-04 14:56:54 +0200

    mailbox viewset and added imports

--- backend/mail_accounts/views.py
+++ backend/mail_accounts/views.py
@@ -1,6 +1,21 @@
+from rest_framework.viewsets import ModelViewSet
+from . import models
+from rest_framework import permissions
+from . import serializers
+from rest_framework.pagination import PageNumberPagination
+
+class CustomPagination(PageNumberPagination):
+    page_size = 25
+    page_size_query_param = 'page_size'
+    max_page_size = 200
+
 class DomainViewSet(ModelViewSet):
     permission_classes = [permissions.AllowAny]
     queryset = models.Domain.objects.all()
     serializer_class = serializers.DomainSerializer
     pagination_class = CustomPagination
 
+class MailBoxViewSet(ModelViewSet):
+    permission_classes = [permissions.AllowAny]
+    queryset = models.MailBox.objects.all()
+    serializer_class = serializers.MailBoxSerializer
