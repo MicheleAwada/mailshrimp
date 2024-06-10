b858f44b979030594b1400801d066ee5d76ebeb9
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-06-10 10:50:43 +0200

    mail config alias

---
+++ frontend/src/routes/mailConfig/alias.jsx
@@ -0,0 +1,10 @@
+import Box from "@mui/material/Box";
+import { Typography } from "@mui/material"
+
+function MailConfigAlias() {
+    return <Box sx={{ height: "100%", p: 4, boxSizing: "border-box" }}>
+    <Typography>hiii</Typography>
+  </Box>
+}
+
+export default MailConfigAlias
