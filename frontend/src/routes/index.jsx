ee164bab58e0470c5228ce8268bbf7f7552bc60f
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-06-11 10:59:09 +0200

    empty index route

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,10 +1,14 @@
+import { Tab, Tabs } from "@mui/material";
+import Box from "@mui/material/Box";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom"
 
 
 
 function Index() {
+
   return (
-    <>
-    </>
+    <></>
   )
 }
 
