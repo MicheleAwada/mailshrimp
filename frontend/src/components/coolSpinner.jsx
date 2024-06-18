6aa85614047242d7ac0dbaaf4b1c32607be641b2
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-06-18 14:33:37 +0200

    spinner component

---
+++ frontend/src/components/coolSpinner.jsx
@@ -0,0 +1,8 @@
+import Box from "@mui/material/Box";
+
+
+export default function CoolSpinner({ color="inherit", sx={}, ...props }) {
+    return (
+        <span className="gb-loader gb-loader--lg gb-loader--inverted"></span>
+    )
+}
