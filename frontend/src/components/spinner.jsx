840dffdc3bed0d6191b28c484d540451482caf96
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-06-23 19:22:46 +0200

    changed spinner component

--- frontend/src/components/spinner.jsx
+++ frontend/src/components/spinner.jsx
@@ -2,7 +2,7 @@ import SpinnerSVG from "../assets/components/spinner/spinner.svg?react"
 
 import SVGIcon from "@mui/material/SvgIcon"
 
-export default function Spinner({ color="inherit", sx={}, ...props }) {
+export default function Spinner({ color="white", sx={}, ...props }) {
     return (
             <SVGIcon component={SpinnerSVG} sx={{ color: color, width: "100%", height: "100%", "@keyframes spin": { from: { transform: "rotate(0deg)" }, to: { transform: "rotate(360deg)" } }, animation: "spin 2s linear infinite", ...sx }} {...props} />
     )
