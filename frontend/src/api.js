e7a1c5b0decf84718d010c0c86ad598cdd267a6e
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-06-24 10:01:16 +0200

    added all new apis to api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,29 +1,97 @@
 import axios from "axios"
 
 // const backendDomain = "https://api.mail.mintyhint.com"
-const backendDomain = "http://127.0.0.1:8000"
+const backendDomain = "http://192.168.1.107:8000"
 
 const api = axios.create({
     baseURL: backendDomain,
 })
-import { actionFromApi, createQuickAPIFunction } from "./api-utils"
 
-const quickAPIFunction = createQuickAPIFunction(api)
+api.interceptors.request.use((config) => {
+    const token = getToken()
+    if (Boolean(token)) {
+        config.headers["Authorization"] = `Token ${token}`
+    }
+    return config
+})
+
+import { actionFromApi, createQuickAPIFunction, loaderFromApi } from "./api-utils"
+import { getDefaultConfig, useConfig } from "./components/configContext"
+
+const quickAPIFunction = createQuickAPIFunction(api, { sleepFor: 0, validate: (response, validateContext) => {
+    if (!response.succeeded && response.error.errorMessage === "Invalid token." && response.extraInfo.serverOnline) {
+        const configState = validateContext?.configState
+        if (configState) {
+            logout({ configState: configState })
+        } else {
+            setToken(null)
+        }    
+    }
+    return response 
+} })
 
 function isAuthenticated() {
-    return Boolean(localStorage.getItem("token"))
+    return Boolean(getToken())
 }
 
 function getToken() {
-    return localStorage.getItem("token")
+    return localStorage.getItem("token") || null
 }
 
 function setToken(token) {
-    localStorage.setItem("token", token)
+    if (Boolean(token)) {
+        localStorage.setItem("token", token)
+    } else {
+        localStorage.removeItem("token")
+    }
 }
 
-export { isAuthenticated, getToken, setToken }
+function getFixUserDataFromUserData(userData) {
+    return {
+        username: userData.username,
+        token: userData.token,
+        is_authenticated: Boolean(userData.token),
+    }
+}
 
-const login = quickAPIFunction({method: "post", link: "/api/login/", })
-const loginAction = actionFromApi({ apiFunction: login})
-export { loginAction }
+
+function setConfigUser({ userData, configState }) {
+    configState[1](oldConfig => ({ ...oldConfig, user: { ...oldConfig.user, ...getFixUserDataFromUserData(userData) } }))
+}
+
+function setFullAuth({ userData, configState }) {
+    setToken(userData.token)
+    setConfigUser({ userData, configState })
+}
+
+function logout({ configState }) {
+    setFullAuth({ userData: getDefaultConfig(true).user, configState: configState })
+}
+
+export { isAuthenticated, getToken, getFixUserDataFromUserData, setConfigUser, setFullAuth, logout }
+
+
+
+const getAccount = quickAPIFunction({method: "get", link: "/api/accounts/get/", })
+
+const loginAPI = quickAPIFunction({method: "post", link: "/api/accounts/login/", })
+const loginAndSaveToken = async (formData) => {
+    const response = await loginAPI(formData)
+    if (response.succeeded) {
+        const userData = response.data
+        setToken(userData.token)
+    }
+    return response
+}
+const loginAction = actionFromApi({ apiFunction: loginAndSaveToken })
+export { getAccount, loginAction, }
+
+
+
+const postDomain = quickAPIFunction({method: "post", link: "/api/mail/domain/", })
+const postDomainAction = actionFromApi({ apiFunction: postDomain })
+const getAllDomains = quickAPIFunction({method: "get", link: "/api/mail/domain/", })
+const getAllDomainsLoader = loaderFromApi({apiFunction: async ({ searchParams }) => {
+    return await getAllDomains(undefined, { params: searchParams }) }
+})
+export { postDomainAction, getAllDomainsLoader }
