6b7741f905147136c8a88d75ca494e1c8599a3e7
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-06-27 19:32:34 +0200

    added routes to main.jsx and more

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App, { BaseAppContext } from './App.jsx'
 import { createTheme, ThemeProvider } from '@mui/material'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { Outlet, Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 
 
 import Index from './routes/index.jsx'
 import Login from './routes/login.jsx'
-import { loginAction } from './api.js'
+import { getAllDomainsLoader, loginAction, postDomainAction } from './api.js'
+import { ConfigProvider } from './components/configContext'
+import MailConfigRoot from './routes/mailConfig/root.jsx'
+import MailConfigDomain from './routes/mailConfig/domain.jsx'
+import MailConfigMailBox from './routes/mailConfig/mailbox.jsx'
+import MailConfigAlias from './routes/mailConfig/alias.jsx'
 
 const route = createBrowserRouter(createRoutesFromElements(
-  <Route path="/" element={<App />}>
-    <Route index element={<Index />} />
-    <Route path="login" element={<Login />} action={loginAction} />
+  <Route path="/" element={<BaseAppContext />}>
+    <Route path="/" element={<App />}>
+      <Route index element={<Index />} />
+      <Route path="login" action={loginAction} />
+      <Route path="config" element={<Outlet />}>
+        <Route path="mail" element={<MailConfigRoot />}>
+          <Route path="domain" action={postDomainAction} loader={getAllDomainsLoader} element={<MailConfigDomain />} />
+          <Route path="mailbox" element={<MailConfigMailBox />} />
+          <Route path="alias" element={<MailConfigAlias />} />
+        </Route>
+      </Route>
+    </Route>
   </Route>
 ))
 
@@ -51,12 +65,16 @@ const themeOptions = {
 
 const theme = createTheme(themeOptions)
 
-
+import { MessagesProvider } from "./components/messages";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <RouterProvider router={route} />
+      <ConfigProvider>
+        <MessagesProvider>
+          <RouterProvider router={route} />
+        </MessagesProvider>
+      </ConfigProvider>
     </ThemeProvider>
   </React.StrictMode>,
 )
